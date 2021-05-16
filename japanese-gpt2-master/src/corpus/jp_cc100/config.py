1e1bdf277d2daed643b9cbc3c71fe0abc8813a21
savage06 <61262439+savage06@users.noreply.github.com>
2021-05-16 11:19:31 +0900

    second

--- japanese-gpt2-master/src/corpus/jp_cc100/config.py
+++
@@ -1,7 +0,0 @@
-class Config(object):
-    def __init__(self):
-        self.corpus_name = "jp_cc100"
-
-        # Management
-        self.raw_data_dir = "../data/jp_cc100/raw_data"
-        self.doc_data_dir = "../data/jp_cc100/doc_data"
